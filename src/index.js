3f354c2f860190b51bceea3e55f71d753ad54c1c
Jekaterina Sjomkina <olympik2010@inbox.lv>
2021-05-29 23:35:31 +0300

    add all files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
+import debounce from 'lodash.debounce';
+import callError from "./js/error";
+import countryListTpl from "./templates/country-list.hbs";
+import countryCardTpl from "./templates/country-card.hbs";
+import API from "./js/fetchCountries";
+import getRefs from "./js/refs";
 import './sass/main.scss';
+
+const refs = getRefs();
+
+const renderCountry = (country) => {
+    if (country.length >= 2 && country.length <= 10) {
+        refs.outputCountry.innerHTML = countryListTpl(country);
+        
+    } else if (country.length === 1) {
+
+        refs.outputCountry.innerHTML = countryCardTpl(...country);
+        
+    } else {
+        callError()
+    }
+};
+
+
+function searchCountry(e) {
+    refs.outputCountry.innerHTML = '';
+    this.value = this.value.replace(/[^\[A-Za-zА]/g, '');
+    const searchQuery = e.target.value;
+    if(searchQuery.length) {
+        API.fetchCountries(searchQuery).then(renderCountry).catch(callError);
+    };
+};
+
+refs.inputCountry.addEventListener('input', debounce(searchCountry, 500));
